b12faeff8e9ef4899a520259112f628ab0d07a58
xiaobolo <xiaobolo@163.com>
2018-03-13 16:34:30 +0800

    添加项目文件。

---
+++ DelegateCommandDemo/Model/PersonModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DelegateCommandDemo.Model
+{
+    class PersonModel : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; NotifyPropertyChanged("name"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string properName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(properName));
+            }
+        }
+    }
+}
---
+++ DelegateCommandDemo/ViewModel/PersonViewModel.cs
@@ -0,0 +1,55 @@
+﻿using DelegateCommandDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DelegateCommandDemo.ViewModel
+{
+    class PersonViewModel
+    {
+        public DelegateCommand ButtonClick { get; set; }
+        public PersonModel Person { get; set; }
+
+        public PersonViewModel()
+        {
+            Person = new PersonModel();
+            ButtonClick = new DelegateCommand();
+            ButtonClick.ExecuteCommand = new Action<object>(EditName);
+            Person.Name = "Xiaobolo";
+        }
+
+        private void EditName(object obj) => Person.Name = "Hermione";
+    }
+
+    internal class DelegateCommand : ICommand
+    {
+        public Action<object> ExecuteCommand = null;
+        public Func<object, bool> CanExecuteCommand = null;
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteCommand != null)
+            {
+                return CanExecuteCommand(parameter);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteCommand?.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
